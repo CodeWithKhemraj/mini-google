5c5dc33363bb7a39ced0657269ac9a8d4459a20c
khemraj <khemrajraj@73gmail.com>
2024-11-01 17:01:47 +0530

    Changed index file diretory and port

--- config/server.js
+++ config/server.js
@@ -7,12 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public'))); // Adjusted to go up one directory
 
+// Use the routes defined in test.js
+app.use("/add-data", require("../scr/routes/test")); // Adjusted path to routes
 
 // Route to serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve index.html from the public directory
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html')); // Adjusted path to index.html
 });
 
 // Start the server
